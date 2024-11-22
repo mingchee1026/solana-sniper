3a3d392fff56d776ef8f18c1a475327511da5924
cheeming <dragon.steal75@gmail.com>
2024-11-23 00:50:11 +0900

    fix: Update jupiter limit order module

---
+++ src/routers/v1/order/limit.ts
@@ -0,0 +1,109 @@
+import bs58 from 'bs58';
+import {Request, Response, NextFunction} from 'express';
+import {Joi} from 'express-validation';
+import {Keypair, PublicKey, VersionedTransaction} from '@solana/web3.js';
+import {NATIVE_MINT} from '@solana/spl-token';
+import * as orderUtils from './utils';
+import * as userUtils from '../user/utils';
+import {priorityFeeEstimate, recentBlockhash, txExecutor} from '../../..';
+import {createLimitTransaction} from '../../../transactions/limit';
+import BigNumber from 'bignumber.js';
+import {Wallet} from '../../../database';
+
+export const createLimit = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const {telegramId} = req.body;
+
+    // Retrieve the user by Telegram ID
+    let user = await orderUtils.getUser(telegramId);
+
+    // Check user has enough balance
+    let inputWalletBalance = new BigNumber(
+      user.defaultWallet!.confirmedBalance
+    ).div(new BigNumber(10).pow(user.selectedInputDecimals)); // If it is SOL token in `buy`
+    if (user.selectedOrderDirection === 'sell') {
+      // In sell it is a non SOL Wallet
+      inputWalletBalance = new BigNumber(
+        (
+          await userUtils.getWalletBalance(user.id, user.selectedInputMint!)
+        ).confirmedBalance
+      );
+    }
+
+    if (
+      (user.selectedOrderDirection === 'buy' &&
+        new BigNumber(user.selectedInputAmount).gt(inputWalletBalance)) ||
+      (user.selectedOrderDirection === 'sell' &&
+        new BigNumber(user.selectedSellAmount).gt(inputWalletBalance))
+    ) {
+      throw new Error("User doesn't have enough balance");
+    }
+
+    // Get the user's secret key and generate keypair
+    const secretKeyString = user.defaultWallet!.key!.privateKey;
+    const keypair = Keypair.fromSecretKey(bs58.decode(secretKeyString));
+
+    // Get the priority fee for the transaction
+    const priorityFee = Math.ceil(
+      priorityFeeEstimate.priorityFeeLevels?.high || 0
+    ); // previous bug: becareful might be float!
+
+    // Get the USD price of the input token (to calculate order value in USD)
+    const tokenUsdPrice =
+      user.selectedOrderDirection === 'buy'
+        ? // TODO get SOL price better
+          (await userUtils.getPoolInfoByTokenAddress(NATIVE_MINT.toBase58())) // ! we only support sol as input in buy orders
+            .priceUsd
+        : user.selectedTokenUsdPrice;
+
+    // Get dca params from users
+    const inAmount = BigInt(user.selectedInputAmount); // BigInt(5_000_000); // buy a total of 5 USDC over 5 days
+    const outAmount = BigInt(user.selectedLimitOutAmount); // BigInt(1_000_000); // buy using 1 USDC each day
+
+    const {
+      order,
+      message,
+      inputATA,
+      outputATA,
+      rawInAmount,
+      inAmountInUsd,
+      ourFeeInLamports,
+    } = await createLimitTransaction(
+      keypair,
+      new PublicKey(user.selectedInputMint!),
+      new PublicKey(user.selectedOutputMint!),
+      inAmount,
+      outAmount,
+      null,
+      user.selectedOrderDirection,
+      tokenUsdPrice,
+      priorityFee
+    );
+
+    // Sign the transaction
+    const transaction = new VersionedTransaction(message.compileToV0Message());
+    transaction.sign([keypair]);
+
+    // Send the transaction and create the order in DB (it is done atomic)
+    await orderUtils.transactionalCreateOrderAndSendToNetwork(
+      user.id,
+      transaction,
+      inputATA,
+      outputATA,
+      rawInAmount,
+      inAmountInUsd.toString(),
+      ourFeeInLamports,
+      priorityFee,
+      txExecutor,
+      recentBlockhash,
+      order,
+      null
+    );
+  } catch (e) {
+    next(e);
+  }
+};
--- src/transactions/limit.ts
+++ src/transactions/limit.ts
@@ -4,13 +4,120 @@ import {
   Transaction,
   Keypair,
   Signer,
+  SystemProgram,
+  TransactionMessage,
   VersionedTransaction,
   ComputeBudgetProgram,
 } from '@solana/web3.js';
+import {
+  getAssociatedTokenAddressSync,
+  TOKEN_PROGRAM_ID,
+} from '@solana/spl-token';
 import fetch from 'cross-fetch';
+import BigNumber from 'bignumber.js';
 import {BlockHash} from '../types';
+import {getConnection, exchangeSolWallet} from '../services';
+import {recentBlockhash, EXCHANGE_FEE} from '..';
 
-export async function getSignedLimitTransaction(
+export async function createLimitTransaction(
+  payer: Signer,
+  inputMint: PublicKey,
+  outputMint: PublicKey,
+  inAmount: bigint,
+  outAmount: bigint,
+  startAt: bigint | null,
+  orderDirection: 'buy' | 'sell',
+  tokenPriceUsd: string,
+  computeUnitPrice: number
+) {
+  if (inAmount === BigInt(0)) {
+    throw new Error(`${payer.publicKey.toBase58()}: Empty balance, can't sell`);
+  }
+
+  // Get DCA transactions and pubkey
+  const {limitTransaction, order} = await getSignedLimitTransactionV2(
+    payer,
+    inputMint,
+    outputMint,
+    inAmount,
+    outAmount,
+    computeUnitPrice
+  );
+
+  // scale the input amount to get the raw amount
+  const inputDecimalPoints = await getTokenDecimals(inputMint);
+  const outputDecimalPoints = await getTokenDecimals(outputMint);
+
+  const inAmountInUsd = new BigNumber(Number(inAmount)).multipliedBy(
+    tokenPriceUsd
+  );
+
+  const rawInAmount = new BigNumber(Number(inAmount)).multipliedBy(
+    new BigNumber(10).pow(inputDecimalPoints)
+  );
+
+  const rawOutAmount = new BigNumber(Number(inAmount)).multipliedBy(
+    new BigNumber(10).pow(outputDecimalPoints)
+  );
+
+  // get associated token addresses
+  const inputATA = getAssociatedTokenAddressSync(
+    inputMint,
+    payer.publicKey,
+    false,
+    TOKEN_PROGRAM_ID
+  );
+  const outputATA = getAssociatedTokenAddressSync(
+    outputMint,
+    payer.publicKey,
+    false,
+    TOKEN_PROGRAM_ID
+  );
+
+  // create priority fee instruction
+  const addPriorityFee = ComputeBudgetProgram.setComputeUnitPrice({
+    microLamports: computeUnitPrice,
+  });
+
+  // calculate out fee
+  const ourFeeInLamports = BigInt(
+    (orderDirection === 'buy'
+      ? rawInAmount
+      : new BigNumber(rawOutAmount.toString())
+    )
+      .multipliedBy(EXCHANGE_FEE)
+      .toFixed(0, BigNumber.ROUND_DOWN)
+  );
+
+  const message = new TransactionMessage({
+    payerKey: payer.publicKey,
+    recentBlockhash: recentBlockhash.blockhash,
+    instructions: [
+      addPriorityFee,
+      ...limitTransaction,
+      SystemProgram.transfer({
+        fromPubkey: payer.publicKey,
+        toPubkey: exchangeSolWallet,
+        lamports: ourFeeInLamports,
+      }),
+    ],
+  });
+
+  const transaction = new VersionedTransaction(message.compileToV0Message());
+  transaction.sign([payer]);
+
+  return {
+    order,
+    message,
+    inputATA,
+    outputATA,
+    rawInAmount,
+    inAmountInUsd,
+    ourFeeInLamports,
+  };
+}
+
+async function getSignedLimitTransactionV1(
   payer: Signer,
   inputMint: PublicKey,
   outputMint: PublicKey,
@@ -63,4 +170,80 @@ export async function getSignedLimitTransaction(
   v0Transaction.sign([payer, base]);
 
   return v0Transaction;
+}
+
+async function getSignedLimitTransactionV2(
+  payer: Signer,
+  inputMint: PublicKey,
+  outputMint: PublicKey,
+  inAmount: bigint,
+  outAmount: bigint,
+  computeUnitPrice: number
+) {
+  if (inAmount === BigInt(0)) {
+    throw new Error(`${payer.publicKey.toBase58()}: Empty balance, can't sell`);
+  }
+
+  // Base key are used to generate a unique order id
+  const base = Keypair.generate();
+
+  // get serialized transactions
+  const {order, tx} = await (
+    await fetch('https://api.jup.ag/limit/v2/createOrder', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        maker: payer.publicKey.toBase58(),
+        payer: payer.publicKey.toBase58(),
+        inputMint: inputMint.toString(),
+        outputMint: outputMint.toString(),
+        params: {
+          makingAmount: inAmount.toString(),
+          takingAmount: outAmount.toString(),
+          expiredAt: null, // new Date().valueOf() / 1000,
+        },
+      }),
+    })
+  ).json();
+
+  // deserialize the transaction
+  const transactionBuf = Buffer.from(tx, 'base64');
+  var transaction = Transaction.from(transactionBuf);
+
+  // // create priority fee instruction
+  // const addPriorityFee = ComputeBudgetProgram.setComputeUnitPrice({
+  //   microLamports: computeUnitPrice,
+  // });
+  // transaction.instructions = [addPriorityFee, ...transaction.instructions];
+
+  return {limitTransaction: transaction.instructions, order};
+
+  // // set recent blockhash
+  // transaction.recentBlockhash = recentBlockhash.blockhash;
+
+  // // Sign the transaction
+  // const v0Transaction = new VersionedTransaction(transaction.compileMessage());
+  // v0Transaction.sign([payer, base]);
+
+  // return v0Transaction;
+}
+
+async function getTokenDecimals(tokenAddress: PublicKey) {
+  const solanaConnection = getConnection();
+  const info = await solanaConnection.getParsedAccountInfo(tokenAddress);
+
+  const accountData = info.value?.data;
+
+  if (accountData && 'parsed' in accountData) {
+    const decimals = accountData.parsed.info?.decimals;
+
+    if (decimals !== undefined) {
+      return decimals;
+    }
+  }
+
+  throw new Error(`${tokenAddress.toBase58()}: Can't find token info`);
 }
