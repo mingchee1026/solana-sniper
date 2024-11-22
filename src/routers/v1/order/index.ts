c2f9d97a122bf0dc1cf22f4ec1aa7ad337a8ad9e
cheeming <dragon.steal75@gmail.com>
2024-11-23 00:50:33 +0900

    fix: Add links for for limit and DCA order

--- src/routers/v1/order/index.ts
+++ src/routers/v1/order/index.ts
@@ -1,2 +1,4 @@
 export * from './new-swap';
 export * from './check-confirmation';
+export * from './limit';
+export * from './dca';
--- src/routers/v1/user/index.ts
+++ src/routers/v1/user/index.ts
@@ -5,3 +5,10 @@ export * from './setSelectedOrderDirection';
 export * from './setSelectedOrderType';
 export * from './setSelectedSellPercent';
 export * from './setSelectedSlippage';
+// Limit Order
+export * from './setSelectedLimitInAmount';
+export * from './setSelectedLimitOutAmount';
+// DCA Order
+export * from './setSelectedDCAInAmount';
+export * from './setSelectedDCAInAmountPerCycle';
+export * from './setSelectedDCACycleSecondsApart';
